d6d6ef31df3709e8328ed2b7bc36e0bf588641db
coder6497 <rty.sem@yandex.ru>
2022-07-01 22:20:12 +0300

    Added login/registration form and autorization functions

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, PasswordField, BooleanField
-from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLAlchemy
 import json
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from wtforms import StringField, SubmitField, SelectField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, EqualTo
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class MorseCoder:
@@ -33,6 +35,8 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = 'povsekakiy_bogdanov'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///crypt_base.db"
 db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -41,6 +45,7 @@ def main():
 
 
 @app.route('/encrypt', methods=["GET", "POST"])
+@login_required
 def encryptor():
     n_word = ''
     form = EncryptForm()
@@ -54,25 +59,61 @@ def encryptor():
 
 
 @app.route('/dict')
+@login_required
 def crypt_dict():
     coder = MorseCoder('')
     return render_template('crypt.html', morse=coder.morse)
 
 
-@app.route('/login')
+@login.user_loader
+def load_user(id):
+    return Users.query.get(id)
+
+
+@app.route('/login', methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    user = Users().query.filter_by(username=form.username.data).first()
+    if user and user.check_password(form.password.data):
+        login_user(user, remember=form.remember.data)
+        return redirect('/')
     return render_template('login.html', form=form)
 
 
-class Users(db.Model):
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
+
+
+@app.route('/regist', methods=['GET', "POST"])
+def registration():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = Users(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return redirect('/')
+    return render_template('registration.html', form=form)
+
+
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String())
     email = db.Column(db.String())
-    password = db.Column(db.String())
+    password_hash = db.Column(db.String())
 
     def __repr__(self):
         return f'<Users {self.id}r>'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class EncryptForm(FlaskForm):
@@ -86,6 +127,14 @@ class LoginForm(FlaskForm):
     password = PasswordField("Пароль", validators=[DataRequired()])
     remember = BooleanField("Запомнить")
     submit = SubmitField("Войти")
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Логин", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Пароль", validators=[DataRequired()])
+    repeat_password = PasswordField("Повторите пароль", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Зарегистрироваться")
 
 
 if __name__ == "__main__":
